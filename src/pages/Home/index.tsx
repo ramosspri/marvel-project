35b91c5a8a95b171a22f95cd77f102b7f9b3d686
yanstivaletti <yanstivalettiesouza15@gmail.com>
2022-03-23 14:22:40 -0300

    CP001 final development of the home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,21 +11,19 @@ import {
   DivInfos,
   DivAlign,
 } from './styles';
-import iron from '../../../public/images/iron.svg';
-import Marvel_Logo from '../../../public/images/Marvel_Logo.svg';
 
 const Home: NextPage = () => {
   return (
     <Container>
       <DivImg>
-        <SvgWrapper src="/images/ironMen.svg" alt="Image of Iron Man" />
+        <SvgWrapper src="/images/ironMan.svg" alt="Image of Iron Man" />
       </DivImg>
       <DivInfos>
         <DivAlign>
           <MarvelLogo src="/images/Marvel_Logo.svg" alt="Logo Marvel" />
           <Title>HEROES LIST</Title>
           <Subtitle>Welcome to our Multiverse of Madness</Subtitle>
-          <HomeButton>HEROES</HomeButton>
+          <HomeButton>ASSEMBLE</HomeButton>
         </DivAlign>
       </DivInfos>
     </Container>
